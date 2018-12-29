fbc200258165a42efec761008f7ca6f5f9497b59
Brandon Baker <brandonbaker40@icloud.com>
2018-12-29 14:49:35 -0500

    adds more visit types to factory

--- spec/factories/visit_type.rb
+++ spec/factories/visit_type.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :visit_type do
-    name { %w[Admission Evaluation].sample }
+    name { %w[Admission Evaluation Followup Recert Reassessment Discharge].sample }
     code { rand.to_s[2..6] }
   end
 end
