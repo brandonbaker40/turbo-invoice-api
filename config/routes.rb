60ac41a3ab4b78996c8f119f5da1b5d65afc6837
Brandon Baker <brandonbaker40@icloud.com>
2018-12-29 14:43:35 -0500

    configures routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,34 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :disciplines
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new, :create]
+  resources :disciplines, :agencies, :visit_types, :visits
+
+  resources :agencies do
+    resources :patients, only: [:index, :new, :create, :show]
+    resources :rates
+    resources :agency_rates
+    resources :visits
+    resources :contract
+    resources :clearances
+  end
+
+  resources :patients do
+    resources :visits
+  end
+
+  resources :users do
+    resources :visits
+    resources :rates
+    resources :clearances
+  end
+
+  resources :visit_types do
+    resources :visits
+    resources :rates
+  end
+
+  resources :disciplines do
+    resources :users, only: [:index, :show]
+  end
 end
